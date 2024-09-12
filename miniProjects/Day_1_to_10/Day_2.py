13372b1c58df2fdb74a0653baf042883626f118a
thirdpartyP <jpannu10@gmail.comgit config --global user.email jpannu10@gmail.comgit config --global user.email jpannu10@gmgit>
2024-09-12 17:57:26 -0400

    Day 2

---
+++ miniProjects/Day_1_to_10/Day_2.py
@@ -0,0 +1,16 @@
+
+
+# Data types
+# Indexing, Negative indexing.
+
+# Integers
+print(123_342_534)
+
+
+# precedence
+# () >  literal ( **  ) >  / *  > - +    (    Left to right )
+
+# round :> This function rounds the passed number.
+
+# f String :
+# print(f"String here {variable_1}, Data here {variable_2}")
