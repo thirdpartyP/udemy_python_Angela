0d7500837aa9e0993b889ecbc8de717d1aedd0c1
thirdpartyP <jpannu10@gmail.comgit config --global user.email jpannu10@gmail.comgit config --global user.email jpannu10@gmgit>
2024-09-11 21:13:04 -0400

    Day 1 : Last Commit

--- miniProjects/Day_1_to_10/Day_1.py
+++ miniProjects/Day_1_to_10/Day_1.py
@@ -1,23 +1,28 @@
+
 print("Day 1")
 
+# print()
+# input()
+# comment
+'''
+# use "CTRL" + "/" to comment the line ( Cursor should be on this line ),
+ Use the same command ( ctrl + / ) to uncomment the line.
+'''
+
+# variables
+# len(string)
+
+# You can install "Thonny" application to Debug you python code step by step.
 
 
 
 
+# Project : Name Band Name Generator.
 
+print("  <---- Welcome to the Band Name Generator. ---- >")
 
+# '\n' will take the input in 2nd line. ( Line Break ) 
+city = input("What's the name of the city you grew up in?  \n")
+pet = input("What's your pet's name? \n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print( city + " " + pet)
