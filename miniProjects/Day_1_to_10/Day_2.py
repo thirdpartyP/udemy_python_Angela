aa62abcdc348676cf47b8ba6338f637070c3879a
thirdpartyP <third.party.pad@gmail.com>
2024-09-13 00:26:35 -0400

    Update Day_2.py
    Added from MAC : web

--- miniProjects/Day_1_to_10/Day_2.py
+++ miniProjects/Day_1_to_10/Day_2.py
@@ -14,3 +14,27 @@ print(123_342_534)
 
 # f String :
 # print(f"String here {variable_1}, Data here {variable_2}")
+
+
+# Tip Calculator Project:
+str1 = "Welcome to Tip calculator "
+str2 = "What was the Total Bill? $ : "
+str3 = "What tip ( % ) would you like to give? 10 | 12 or 15 " 
+str4 = "How many people to Split the bill with? "
+resultDisplay = f"Each Person should Amount : $ "
+
+print(str1)
+totalBill = float(input(str2))
+perTip = int(input(str3))
+split = int(input(str4))
+
+result = (totalBill * (  perTip / 100  ) ) + totalBill
+result = round(result,2)
+
+if split > 0:
+    result = result / split
+    print("Each need to pay $ :" ,result)
+
+
+print("Total Bill :  ", result)
+print("Tipeed  %", perTip)
